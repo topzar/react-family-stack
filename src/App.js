3a3d859cf41e106ae9287c622738c2d74d77d3ec
oyghan <742163033@qq.com>
2019-10-17 13:13:20 +0800

    feat: 引入antd组件库

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import Button from 'antd/es/button';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        hello react
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h3>hello react</h3>
+            <Button type="primary">Button</Button>
+        </div>
+    );
 }
 
 export default App;
